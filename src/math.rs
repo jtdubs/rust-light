abb189f68f9738d1ca25a2b77d56b17c4d838fc3
Justin Dubs <jtdubs@gmail.com>
2014-10-14 17:29:09 -0400

    more of math

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,7 @@
+extern crate std;
+
+use std::iter::{iterate};
+
 pub fn quadratic(a : f64, b : f64, c : f64) -> Option<[f64, ..2]> {
     let d = b*b - 4f64*a*c;
     if d < 0f64 {
@@ -11,5 +15,37 @@ pub fn quadratic(a : f64, b : f64, c : f64) -> Option<[f64, ..2]> {
 }
 
 pub fn radical_inverse(n : i64, b : i64) -> f64 {
-    0f64
+    helper(0f64, n, 1f64 / (b as f64), 1f64 / (b as f64), b)
+}
+
+fn helper(r : f64, i : i64, inv_bi : f64, inv_base : f64, b : i64) -> f64 {
+    if i == 0 {
+        r
+    } else {
+        let di = i % b;
+        helper(r + ((di as f64) * inv_bi), ((i as f64) * inv_base).trunc() as i64, inv_bi * inv_base, inv_base, b)
+    }
+}
+
+pub fn van_der_corput(n : u32, scramble : u32) -> f64 {
+    let num = ((reverse_bits(n) ^ scramble) >> 8) & 0xFFFFFF;
+    let denom = 1u32 << 24;
+    (num as f64) / (denom as f64)
+}
+
+pub fn sobol(n : u32, scramble : u32) -> f64 {
+    let vs = std::iter::iterate(1u32 << 31, |x| { x ^ (x >> 1) }).take(32);
+    let ns = range(0u, 32u).map(|s| { (n >> s) & 1 });
+    let s = ns.zip(vs).map(|(a, b)| { a * b }).fold(scramble, |a, b| { a ^ b });
+    (((s >> 8) & 0xFFFFFF) as f64) / ((1u32 << 24) as f64)
+}
+
+pub fn reverse_bits(n : u32) -> u32 {
+    let mut r = n;
+    r = (r << 16) | (r >> 16);
+    r = (r & 0x00FF00FF) >> 8 | (r & 0xFF00FF00) << 8;
+    r = (r & 0x0F0F0F0F) >> 4 | (r & 0xF0F0F0F0) << 4;
+    r = (r & 0x33333333) >> 2 | (r & 0xCCCCCCCC) << 2;
+    r = (r & 0x55555555) >> 1 | (r & 0xAAAAAAAA) << 1;
+    r
 }
