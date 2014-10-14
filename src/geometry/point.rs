e2a8d1a8667c9618846a0321a48067ae52032c88
Justin Dubs <jtdubs@gmail.com>
2014-10-14 19:39:23 -0400

    point tests

--- src/geometry/point.rs
+++ src/geometry/point.rs
@@ -54,7 +54,7 @@ impl Point {
     }
 
     pub fn sub_p(&self, o : &Point) -> Vector {
-        Vector::new(o.x - self.x, o.y - self.y, o.z - self.z)
+        Vector::new(self.x - o.x, self.y - o.y, self.z - o.z)
     }
 }
 
@@ -89,4 +89,79 @@ impl Transformable for Point {
         let c = self.clone();
         self.clone_from(&t.transformation_matrix().mul_p(&c))
     }
+}
+
+#[test]
+fn test_accessors() {
+    assert_eq!(Point::new(1f64, 2f64, 3f64).x, 1f64);
+    assert_eq!(Point::new(1f64, 2f64, 3f64).y, 2f64);
+    assert_eq!(Point::new(1f64, 2f64, 3f64).z, 3f64);
+}
+
+#[test]
+fn test_equality() {
+    assert!(Point::origin() == Point::origin());
+    assert!(Point::origin() == Point::new(0f64, 0f64, 0f64));
+    assert!(Point::origin() != Point::new(1f64, 0f64, 0f64));
+    assert!(Point::origin() != Point::new(0f64, 1f64, 0f64));
+    assert!(Point::origin() != Point::new(0f64, 0f64, 1f64));
+    assert!(Point::new(1f64, 0f64, 0f64) == Point::new(1f64, 0f64, 0f64));
+    assert!(Point::new(1f64, 0f64, 0f64) != Point::new(0f64, 1f64, 0f64));
+}
+
+#[test]
+fn test_distance() {
+    let o = Point::origin();
+    let d1 = o.add_v(&Vector::unit_x());
+    let d2 = d1.add_v(&Vector::unit_y());
+
+    assert_eq!(o.distance(&o), 0f64);
+    assert_eq!(o.distance(&d1), 1f64);
+    assert_eq!(d1.distance(&d2), 1f64);
+    assert_eq!(o.distance(&d2), 2f64.sqrt());
+}
+
+#[test]
+fn test_vector_to_from() {
+    let o = Point::origin();
+    let d1 = o.add_v(&Vector::unit_x());
+    let d2 = d1.add_v(&Vector::unit_y());
+
+    assert_eq!(o.vector_to(&d1), Vector::unit_x());
+    assert_eq!(d1.vector_to(&d2), Vector::unit_y());
+    assert_eq!(o.vector_to(&d2), Vector::unit_x().add_v(&Vector::unit_y()));
+
+    assert_eq!(o.vector_from(&d1), Vector::unit_x().reverse());
+    assert_eq!(d1.vector_from(&d2), Vector::unit_y().reverse());
+    assert_eq!(o.vector_from(&d2), Vector::unit_x().add_v(&Vector::unit_y()).reverse());
+}
+
+#[test]
+fn test_add() {
+    assert_eq!(Point::new(1f64, 0f64, 0f64).add_v(&Vector::unit_x()), Point::new(2f64, 0f64, 0f64));
+    assert_eq!(Point::new(1f64, 0f64, 0f64).add_v(&Vector::unit_y()), Point::new(1f64, 1f64, 0f64));
+    assert_eq!(Point::new(1f64, 0f64, 0f64).add_v(&Vector::unit_z()), Point::new(1f64, 0f64, 1f64));
+
+    let mut v = Point::new(1f64, 0f64, 0f64);
+    v.add_self_v(&Vector::unit_x());
+    v.add_self_v(&Vector::unit_y());
+    assert_eq!(v, Point::new(2f64, 1f64, 0f64));
+}
+
+#[test]
+fn test_sub() {
+    assert_eq!(Point::new(1f64, 0f64, 0f64).sub_v(&Vector::unit_x()), Point::new(0f64, 0f64, 0f64));
+    assert_eq!(Point::new(1f64, 0f64, 0f64).sub_v(&Vector::unit_y()), Point::new(1f64, -1f64, 0f64));
+    assert_eq!(Point::new(1f64, 0f64, 0f64).sub_v(&Vector::unit_z()), Point::new(1f64, 0f64, -1f64));
+
+    let mut v = Point::new(1f64, 0f64, 0f64);
+    v.sub_self_v(&Vector::unit_x());
+    v.sub_self_v(&Vector::unit_y());
+    assert_eq!(v, Point::new(0f64, -1f64, 0f64));
+}
+
+#[test]
+fn test_sub_p() {
+    assert_eq!(Point::new(1f64, 0f64, 0f64).sub_p(&Point::origin()), Vector::unit_x());
+    assert_eq!(Point::new(1f64, 0f64, 0f64).sub_p(&Point::new(1f64, 1f64, 1f64)), Vector::new(0f64, -1f64, -1f64));
 }
