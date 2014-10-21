b05fef729f6b1d4fee9456842631357101dd6803
Justin Dubs <jtdubs@gmail.com>
2014-10-21 16:48:36 +0000

    add cone

--- src/shape.rs
+++ src/shape.rs
@@ -2,6 +2,7 @@ use geometry::transform::{Transform,Trans,TransMut};
 use geometry::aabb::AABB;
 use geometry::ray::Ray;
 use geometry::point::Point;
+use math::quadratic;
 
 pub trait Shape<'a> : Trans {
     fn get_transform(&'a self) -> &'a Transform;
@@ -97,6 +98,64 @@ impl<'a> Shape<'a> for Box {
 
             if t0 >= 0f64 { res.push(t0); }
             if t1 >= 0f64 { res.push(t1); }
+        }
+        res
+    }
+}
+
+pub struct Cone {
+    t : Transform,
+    r : f64,
+    h : f64,
+}
+
+impl Cone {
+    pub fn new(r : f64, h : f64) -> Cone {
+        Cone { t: Transform::identity(), r: r, h: h }
+    }
+
+    pub fn unit() -> Cone {
+        Cone::new(1f64, 1f64)
+    }
+}
+
+impl Trans for Cone {
+    fn transform(&self, t : &Transform) -> Cone {
+         Cone { t: t.compose(&self.t), r: self.r, h: self.h }
+    }
+}
+
+impl TransMut for Cone {
+    fn transform_self(&mut self, t : &Transform) {
+        self.t = t.compose(&self.t);
+    }
+}
+
+impl<'a> Shape<'a> for Cone {
+    fn get_transform(&'a self) -> &'a Transform {
+        &self.t
+    }
+
+    fn bound(&self) -> AABB {
+        AABB::for_points([Point::new(-self.r, -self.r, 0f64), Point::new(self.r, self.r, self.h)])
+    }
+
+    fn surface_area(&self) -> f64 {
+        self.r * (self.r * self.r + self.h * self.h).sqrt() * Float::pi()
+    }
+
+    fn intersections(&self, r : &Ray) -> Vec<f64> {
+        let mut res = Vec::new();
+        let r2 = r.transform(&self.get_transform().inverse());
+        let a = (self.h*self.h*r2.direction.x*r2.direction.x + self.h*self.h*r2.direction.y*r2.direction.y) / (self.r*self.r) + (-r2.direction.z*r2.direction.z);
+        let b = (2f64*self.h*self.h*r2.origin.x*r2.origin.x + 2f64*self.h*self.h*r2.origin.y*r2.origin.y) / (self.r*self.r) + (-2f64*r2.origin.z*r2.direction.z + 2f64*r2.direction.z*self.h);
+        let c = (self.h*self.h*r2.origin.x*r2.origin.x + self.h*self.h*r2.origin.y*r2.origin.y) / (-r2.origin.z*r2.origin.z + 2f64*r2.origin.z*self.h - self.h*self.h);
+        match quadratic(a, b, c) {
+            None => { }
+            Some([t1, t2]) => {
+                if t1 > 0f64 && r2.at_time(t1).z >= 0f64 && r2.at_time(t1).z <= self.h { res.push(t1); };
+                if t2 > 0f64 && r2.at_time(t2).z >= 0f64 && r2.at_time(t2).z <= self.h { res.push(t2); };
+            }
         }
         res
     }
