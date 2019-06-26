e6644300f21c9e023f17efeac2707f82e3890f3f
Justin Dubs <jtdubs@gmail.com>
2019-06-26 16:16:50 +0000

    resurrect sampler test

--- src/bin/sampler_test.rs
+++ src/bin/sampler_test.rs
@@ -1,18 +1,16 @@
-// use light::sampler::Sampler;
+use light::sampler::*;
 
 pub fn main() {
-    // let mut s = Sampler::new();
-
     println!("clf");
-    // draw(1, 2, 3, 1, None, "Centers",    &s.strata_centers_2d(10, 10));
-    // draw(1, 2, 3, 2, None, "Uniform",    &s.uniform_2d(100));
-    // draw(1, 2, 3, 3, None, "Strata",     &s.strata_2d(10, 10));
-    // draw(1, 2, 3, 4, None, "LHC",        &s.lhc_2d(100));
-    // draw(1, 2, 3, 5, None, "Halton",     &s.halton_2d(100));
-    // draw(1, 2, 3, 6, None, "Hammersley", &s.hammersley_2d(100));
+    draw(1, 2, 3, 1, None, "Centers",    Box::new(StrataCentersSampler2D::new(100, 100)));
+    draw(1, 2, 3, 2, None, "Uniform",    Box::new(UniformSampler2D::new(100)));
+    draw(1, 2, 3, 3, None, "Strata",     Box::new(StrataSampler2D::new(100, 100)));
+    draw(1, 2, 3, 4, None, "LHC",        Box::new(LHCSampler2D::new(100)));
+    draw(1, 2, 3, 5, None, "Halton",     Box::new(HaltonSampler2D::new(100)));
+    draw(1, 2, 3, 6, None, "Hammersley", Box::new(HammersleySampler2D::new(100)));
 }
 
-fn draw(f : u32, w : u32, h : u32, ix : u32, grid : Option<(i32, i32)>, title : &str, samples : &Vec<(f32, f32)>) {
+fn draw(f : u32, w : u32, h : u32, ix : u32, grid : Option<(i32, i32)>, title : &str, mut samples : Box<impl Sampler2D>) {
     println!("figure ({});", f);
     println!("subplot ({}, {}, {});", w, h, ix);
     match grid {
@@ -33,12 +31,12 @@ fn draw(f : u32, w : u32, h : u32, ix : u32, grid : Option<(i32, i32)>, title : 
     println!("axis (\"off\", \"nolabel\");");
     println!("title (\"{}\");", title);
     print!("xs = [");
-    for &(x, _) in samples.iter() {
+    for &(x, _) in samples.get_samples().iter() {
         print!("{}, ", x);
     }
     println!("];");
     print!("ys = [");
-    for &(_, y) in samples.iter() {
+    for &(_, y) in samples.get_samples().iter() {
         print!("{}, ", y);
     }
     println!("];");
