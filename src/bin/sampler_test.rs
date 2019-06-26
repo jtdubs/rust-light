9d7955154770ff6c5d8187de937f65a19076132c
Justin Dubs <jtdubs@gmail.com>
2019-06-26 14:33:45 +0000

    temporarily disable sampler tests

--- src/bin/sampler_test.rs
+++ src/bin/sampler_test.rs
@@ -1,15 +1,15 @@
-use light::sampler::Sampler;
+// use light::sampler::Sampler;
 
 pub fn main() {
-    let mut s = Sampler::new();
+    // let mut s = Sampler::new();
 
     println!("clf");
-    draw(1, 2, 3, 1, None, "Centers",    &s.strata_centers_2d(10, 10));
-    draw(1, 2, 3, 2, None, "Uniform",    &s.uniform_2d(100));
-    draw(1, 2, 3, 3, None, "Strata",     &s.strata_2d(10, 10));
-    draw(1, 2, 3, 4, None, "LHC",        &s.lhc_2d(100));
-    draw(1, 2, 3, 5, None, "Halton",     &s.halton_2d(100));
-    draw(1, 2, 3, 6, None, "Hammersley", &s.hammersley_2d(100));
+    // draw(1, 2, 3, 1, None, "Centers",    &s.strata_centers_2d(10, 10));
+    // draw(1, 2, 3, 2, None, "Uniform",    &s.uniform_2d(100));
+    // draw(1, 2, 3, 3, None, "Strata",     &s.strata_2d(10, 10));
+    // draw(1, 2, 3, 4, None, "LHC",        &s.lhc_2d(100));
+    // draw(1, 2, 3, 5, None, "Halton",     &s.halton_2d(100));
+    // draw(1, 2, 3, 6, None, "Hammersley", &s.hammersley_2d(100));
 }
 
 fn draw(f : u32, w : u32, h : u32, ix : u32, grid : Option<(i32, i32)>, title : &str, samples : &Vec<(f32, f32)>) {
