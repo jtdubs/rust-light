a8104ab1fd462c32491619d38a429937f6b4f6e0
Justin Dubs <jtdubs@gmail.com>
2019-06-28 17:02:52 +0000

    make timing script choose specific cmd-line options

--- time.sh
+++ time.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 cargo build --release --bin rusty_light
-time ./target/release/rusty_light
+time ./target/release/rusty_light --camera perspective --filter gaussian --fov 60 --output out/bench.png --res 1080p --samples 16
